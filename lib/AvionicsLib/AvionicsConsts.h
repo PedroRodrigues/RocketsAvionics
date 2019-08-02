73229872492ec72b30fce9874de48953bbcf5d79
diegorodrigues93 <diegorodrigues-5@hotmail.com>
2019-08-02 00:09:46 -0300

    BMP280 Address

--- lib/AvionicsLib/AvionicsConsts.h
+++ lib/AvionicsLib/AvionicsConsts.h
@@ -1,6 +1,7 @@
 #ifndef AvionicsConsts_h
 #define AvionicsConsts_h
 
-#define MPU_ADDRESS                   0x68
+#define MPU_ADDRESS                  0x68
+#define BMP280_ADDRESS               0x77
 
 #endif
